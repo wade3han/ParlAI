7d4869525b9343f3cda5f341f1778ad2d4a3258e
Seungju Han <wade3han@gmail.com>
2023-01-19 19:06:11 +0900

    test inference

--- parlai/core/params.py
+++ parlai/core/params.py
@@ -700,6 +700,7 @@ class ParlaiParser(argparse.ArgumentParser):
                 'test',
                 'test:stream',
                 'test_first_turn',
+                'test_inference',
             ],
             help='choose from: train, train:ordered, valid, test. to stream '
             'data add ":stream" to any option (e.g., train:stream). '
